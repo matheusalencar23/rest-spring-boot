829405b1044e47e69ffff2c52282580a5d12fb90
Matheus Alencar <matheusalencar6942@gmail.com>
2025-02-05 18:09:41 -0300

    05 - First Steps with Spring Boot

---
+++ src/test/java/io/github/matheusalencar23/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheusalencar23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
